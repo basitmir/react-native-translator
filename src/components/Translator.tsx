fc66127eca3e6249b7bb1f5d99926a21b3441903
Basit Mir <abasit@noon.com>
2026-06-13 15:09:57 +0530

    chore: remove packageManager field, fix prettier, regenerate yarn.lock v1

--- src/components/Translator.tsx
+++ src/components/Translator.tsx
@@ -30,17 +30,20 @@ function Translator<T extends TranslatorType = 'google'>(
     [translator, from, to, value],
   );
 
-  const onMessage = useCallback((event: WebViewMessageEvent) => {
-    const result = event.nativeEvent.data;
-    if (
-      !result ||
-      result === LOADING_MESSSAGE ||
-      result.includes('Enter a URL')
-    ) {
-      return;
-    }
-    onTranslated(result);
-  }, [onTranslated]);
+  const onMessage = useCallback(
+    (event: WebViewMessageEvent) => {
+      const result = event.nativeEvent.data;
+      if (
+        !result ||
+        result === LOADING_MESSSAGE ||
+        result.includes('Enter a URL')
+      ) {
+        return;
+      }
+      onTranslated(result);
+    },
+    [onTranslated],
+  );
 
   // set value throttled
   const throttledSetValue = useMemo(() => _.debounce(setValue, 100), []);
